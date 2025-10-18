1fbc675211ac0e234b7f5e9b9dbedfad3b6c37e2
Martin Oberzalek <kingleo@gmx.at>
2025-10-18 23:16:17 +0200

    copied from stips

---
+++ src/bindtypes.cc
@@ -0,0 +1,359 @@
+#include "bindtypes.h"
+
+const std::string JANEIN::STYPES[] = { "NEIN", "JA" };
+
+Forkey::Forkey( DBBindType *base, 
+		  DBType *field_,
+		  const std::string & target_table_,
+		  const std::string & target_field_ )
+  : own_table( base->get_table() ),
+	field( field_->get_name() ),
+	target_table( target_table_ ),
+	target_field( target_field_ )
+{}
+
+BASE::BASE( const std::string & name, DBBindType *base )
+  : DBBindType( name ),
+	idx( base, "idx" ),
+	gueltig( base, "gueltig" ),
+	gueltig_von( base, "gueltig_von" ),
+	gueltig_bis( base, "gueltig_bis" ),
+	hist_an_zeit( base, "hist_an_zeit" ),
+	hist_an_user( base, "hist_an_user", USER_NAME_LEN ),
+	hist_ae_zeit( base, "hist_ae_zeit" ),
+	hist_ae_user( base, "hist_ae_user", USER_NAME_LEN ),
+	hist_lo_zeit( base, "hist_lo_zeit" ),
+	hist_lo_user( base, "hist_lo_user", USER_NAME_LEN )
+{
+  gueltig.data = JANEIN::JA;
+}
+
+GRP_UST::GRP_UST()
+  : BASE( "GRP_UST", this ),
+	name( this, "name", NAME_LEN ),
+	bezeichnung( this, "bezeichnung", BEZEICHNUNG_LEN ),
+	bezeichnung2( this, "bezeichnung2", BEZEICHNUNG_LEN ),
+	ust( this, "ust" )
+{}
+
+ART::ART()
+  : BASE( "ART", this ),
+	artnr( this, "artnr", NAME_LEN ),
+	bezeichnung( this, "bezeichnung", BEZEICHNUNG_LEN ),
+	bezeichnung2( this, "bezeichnung2", BEZEICHNUNG_LEN ),
+	gruppe_ust( this, "gruppe_ust", NAME_LEN ),
+	gruppe_ust_idx( this, "gruppe_ust_idx" )
+{
+  add_key( new Forkey( this, &gruppe_ust_idx, "GRP_UST", "idx" ) );
+  add_key( new Forkey( this, &gruppe_ust, "GRP_UST", "name" ) );
+}
+
+PREIS::PREIS()
+  : BASE( "PREIS", this ),
+	art_artnr( this, "art_artnr", NAME_LEN ),
+	art_idx( this, "art_idx" ),
+	preis( this, "preis" ),
+	bezeichnung( this, "bezeichnung", BEZEICHNUNG_LEN ),
+	bezeichnung2( this, "bezeichnung2", BEZEICHNUNG_LEN ),
+	gutschein( this, "gutschein" ),
+	menuepreis( this, "menuepreis" )
+{
+  gutschein.data = JANEIN::NEIN;
+  menuepreis.data = JANEIN::NEIN;
+
+  add_key( new Forkey( this, &art_artnr, "ART", "artnr" ) );
+  add_key( new Forkey( this, &art_idx, "ART", "idx" ) );
+}
+
+USER::USER()
+  : BASE( "USER", this ),
+	login( this, "login", NAME_LEN ),
+	vor_name( this, "vor_name", NAME_LEN ),
+	nach_name( this, "nach_name", NAME_LEN ),
+	passwd( this, "passwd", NAME_LEN )
+{
+}
+
+GRP_ART::GRP_ART()
+  : DBBindType( "GRP_ART" ),
+	idx( this, "idx" ),
+	name( this, "name", NAME_LEN ),
+	bezeichnung( this, "bezeichnung", BEZEICHNUNG_LEN ),
+	bezeichnung2( this, "bezeichnung2", BEZEICHNUNG_LEN ),
+	art_artnr( this, "art_artnr", NAME_LEN ),
+	art_idx( this, "art_idx" ) 
+{
+  add_key( new Forkey( this, &art_artnr, "ART", "artnr" ) );
+  add_key( new Forkey( this, &art_idx, "ART", "idx" ) );
+}
+
+LAYOUTK::LAYOUTK()
+  : DBBindType( "LAYOUTK" ),
+	idx( this, "idx" ),
+	name( this, "name", LAYOUT_NAME_LEN ),
+	width( this, "width" ),
+	height( this, "height" ),
+	color( this, "color" )
+{}
+
+LAYOUTP::LAYOUTP()
+  : DBBindType( "LAYOUTP" ),
+	idx( this, "idx" ),
+	layout_idx( this, "layout_idx" ),
+	label_name( this, "label_name", LAYOUT_NAME_LEN ),
+	label_x( this, "label_x" ),
+	label_y( this, "label_y" ),
+	label_width( this, "label_width" ),
+	label_height( this, "label_height" ),
+	label_color( this, "label_color" ),
+	label_icon( this, "label_icon", LAYOUT_NAME_LEN )
+{
+  add_key( new Forkey( this, &layout_idx, "LAYOUTK", "idx" ) );
+}
+
+
+BONLINE::BONLINE()
+  : BASE( "BONLINE", this ),
+	user( this, "user", USER_NAME_LEN ),
+	user_idx( this, "user_idx" ),
+	artnr( this, "artnr", NAME_LEN ),
+	artnr_idx( this, "artnr_idx" ),
+	bill_printed( this, "bill_printed" ),
+	bill_number( this, "bill_number", BILL_NAME_LEN ),
+	layoutk_idx( this, "layoutk_idx" ),
+	layoutk_name( this, "layoutk_name", LAYOUT_NAME_LEN ),
+	layoutp_idx( this, "layoutp_idx" ),
+	layoutp_label_name( this, "layoutp_label_name", LAYOUT_NAME_LEN ),
+	tagesabschluss( this, "tagesabschluss" ),
+	abschl_zeit( this, "abschl_zeit" ),
+	abschl_user_idx( this, "abschl_user_idx" ),
+	abschl_user_name( this, "abschl_user_name", USER_NAME_LEN ),
+	storniert( this, "storniert" ),
+	storno_user( this, "storno_user" ),
+	storno_user_idx( this, "storno_user_idx" ),
+	anzahl( this, "anzahl" ),
+	preis_idx( this, "preis_idx" ),
+	preis( this, "preis" ),
+	bar_bezahlt( this, "bar_bezahlt" ),
+	sonst_bezahlt( this, "sonst_bezahlt" ),
+	sonst_art_name( this, "sonst_art_name", NAME_LEN ),
+	sonst_art_idx( this, "sonst_art_idx" ),
+	bezahlt( this, "bezahlt" )
+{
+  bill_printed = JANEIN::NEIN;
+  tagesabschluss = JANEIN::NEIN;
+  storniert = JANEIN::NEIN;
+  bezahlt = JANEIN::NEIN;
+
+  add_key( new Forkey( this, &user, "USER", "login" ) );
+  add_key( new Forkey( this, &user_idx, "USER", "idx" ) );
+  add_key( new Forkey( this, &layoutk_idx, "LAYOUTK", "idx" ) );
+  add_key( new Forkey( this, &layoutk_name, "LAYOUTK", "name" ) );
+  add_key( new Forkey( this, &layoutp_idx, "LAYOUTP", "idx" ) );
+  add_key( new Forkey( this, &layoutp_label_name, "LAYOUTP", "label_name" ) );
+  // add_key( new Forkey( this, &preis_idx, "PREIS", "idx" ) );
+  // add_key( new Forkey( this, &preis, "PREIS", "preis" ) );
+  // add_key( new Forkey( this, &sonst_art_name, "SONST_ART", "name" ) );
+  // add_key( new Forkey( this, &sonst_art_idx, "SONST_ART", "idx" ) );
+}
+  
+
+SONST_ART::SONST_ART()
+  : BASE( "SONST_ART", this ),
+	name( this, "name", NAME_LEN ),
+	bezeichnung( this, "bezeichnung", BEZEICHNUNG_LEN ),
+	bezeichnung2( this, "bezeichnung2", BEZEICHNUNG_LEN ),
+	wert( this, "wert" )
+{
+
+}
+
+LANG::LANG()
+  : DBBindType( "LANG" ),
+	TableType( TableType::MYISAM ),
+	idx( this, "idx" ),
+	system( this, "system", SYSTEM_NAME_LEN ),
+	type( this, "type", TYPE_NAME_LEN ),
+	orig( this, "orig", TRANS_NAME_LEN ),
+	trans( this, "trans", TRANS_NAME_LEN )
+{
+  
+}
+
+
+CONFIG::CONFIG()
+  : DBBindType( "CONFIG" ),
+	TableType( TableType::MYISAM ),
+	idx( this, "idx" ),
+	system( this, "system", CONFIG_SYSTEM_LEN ),
+	key( this, "key", CONFIG_KEY_LEN ),
+	value( this, "value", CONFIG_VALUE_LEN ),
+	descr( this, "descr", CONFIG_DESCR_LEN )
+{
+  
+}
+	
+
+std::string create_sql_statement( DBBindType *table, std::vector< Ref<Forkey> > & forkeys )
+{
+  std::string s;
+
+  s = "DROP TABLE IF EXISTS `" + table->get_table() + "`;\n";
+  s += "CREATE TABLE `" + table->get_table() + '`';
+  s += " (\n ";
+  
+  std::vector<DBType*> tl = table->get_types();
+
+  for( unsigned i = 0; i < tl.size(); i++ ) 
+	{
+	  if( i > 0 ) 
+		{
+		  s += ",\n";
+		}
+
+	  s += "`" + tl[i]->get_name() + "` ";
+
+	  switch( tl[i]->type ) 
+		{
+		case DBType::TYPE::INT:
+		  s += "INT ";
+		  break;
+
+		case DBType::TYPE::DOUBLE:
+		  s += "DOUBLE ";
+		  break;
+
+		case DBType::TYPE::VARCHAR:
+		  s += format("VARCHAR(%s) CHARACTER SET utf8 COLLATE utf8_general_ci ", tl[i]->get_size() );
+		  break;
+
+		case DBType::TYPE::ENUM:
+		  {
+			s += "ENUM ( ";
+			
+			DBTypeEnumBase *eb = dynamic_cast<DBTypeEnumBase*>( tl[i] );
+			
+			if( eb ) 
+			  {
+				for( unsigned j = eb->get_first_case(); j <= eb->get_last_case(); j++ )
+				  {
+					if( j > eb->get_first_case() )
+					  s += ", ";
+					
+					s += format( "'%s'", eb->get_case(j));
+				  }			  
+			  }
+			
+			s += " ) CHARACTER SET utf8 COLLATE utf8_general_ci ";
+		  }
+		  break;
+
+		case DBType::TYPE::DATETIME:
+		  s += "DATETIME ";
+		  break;
+
+		case DBType::TYPE::FIRST__:
+		case DBType::TYPE::LAST__:
+		  break;
+		}
+
+	  s += "NOT NULL ";
+	}
+
+  std::string engine = "INNODB";
+
+  TableType *tt = dynamic_cast<TableType*>(table);
+
+  if( tt )
+	{
+	  switch( tt->table_type )
+		{
+		case TableType::MYISAM: 
+		  engine = "MYISAM";
+		  break;
+		case TableType::INNODB:
+		  engine = "INNODB";
+		  break;
+		}
+	}
+
+  s += format( "\n) ENGINE = %s;\n", engine );
+  s += format("ALTER TABLE `%s` ADD PRIMARY KEY ( `idx` );\n",table->get_table()); 
+  s += format("ALTER TABLE `%s` CHANGE `idx` `idx` INT( 11 ) NOT NULL AUTO_INCREMENT;\n",
+			  table->get_table());
+  s += format("ALTER TABLE `%s`  DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;\n",table->get_table()); 
+
+  Forkeys *fk = dynamic_cast<Forkeys*>( table );
+
+  if( fk )
+	{
+	  forkeys.insert( forkeys.end(), fk->forkeys.begin(), fk->forkeys.end() );
+	}
+  
+  return s;
+}
+
+std::string create_sql()
+{
+  std::string s;
+  GRP_UST grp_ust;
+  USER user;
+  ART art;
+  GRP_ART grp_art;
+  LAYOUTK layoutk;
+  LAYOUTP layoutp;
+  BONLINE bonline;
+  PREIS preis;
+  SONST_ART sonst_art;
+  LANG lang;
+  CONFIG config;
+
+  std::vector< Ref<Forkey> > forkeys;
+
+  s += create_sql_statement( &grp_ust, forkeys );
+  s += create_sql_statement( &user, forkeys  );
+  s += create_sql_statement( &art, forkeys  );
+  s += create_sql_statement( &grp_art, forkeys  );
+  s += create_sql_statement( &layoutk, forkeys  );
+  s += create_sql_statement( &layoutp, forkeys  );
+  s += create_sql_statement( &bonline, forkeys  );
+  s += create_sql_statement( &preis, forkeys  );
+  s += create_sql_statement( &sonst_art, forkeys  );
+  s += create_sql_statement( &lang, forkeys  );
+  s += create_sql_statement( &config, forkeys  );
+
+  // notwendige indexe anlegen
+  for( unsigned i = 0; i < forkeys.size(); i++ )
+	{
+	  if( forkeys[i]->target_field != "idx" ) 
+		{
+		  std::string ss;
+
+		  ss = format( "ALTER TABLE `%s` ADD INDEX `%s_%s_%s`(`%s`);\n",
+					   forkeys[i]->target_table,
+					   "idx",
+					   forkeys[i]->target_table,
+					   forkeys[i]->target_field,
+					   forkeys[i]->target_field );
+
+		  // Damit wir nicht 2 mal den selben index anlegen. 
+		  if( s.find( ss ) == std::string::npos )
+			{
+			  s += ss;
+			}
+		}
+	}
+
+  // Forkeys anlegen
+  for( unsigned i = 0; i < forkeys.size(); i++ )
+	{
+	  s += format( "ALTER TABLE `%s` add FOREIGN KEY (`%s`) REFERENCES `%s`(`%s`);\n",
+				   forkeys[i]->own_table,
+				   forkeys[i]->field,
+				   forkeys[i]->target_table,
+				   forkeys[i]->target_field );
+	}
+
+  return s;
+}
+
---
+++ src/bindtypes.h
@@ -0,0 +1,320 @@
+#ifndef BINDTYPES_H
+#define BINDTYPES_H
+
+#include "tools/dbi.h"
+
+using namespace Tools;
+
+const unsigned USER_NAME_LEN=20;
+const unsigned TIME_LEN=19;
+const unsigned NAME_LEN=20;
+const unsigned LAYOUT_NAME_LEN=20;
+const unsigned BEZEICHNUNG_LEN=50;
+const unsigned BILL_NAME_LEN=20;
+const unsigned SYSTEM_NAME_LEN=10;
+const unsigned TRANS_NAME_LEN=100;
+const unsigned TYPE_NAME_LEN=20;
+const unsigned CONFIG_SYSTEM_LEN=20;
+const unsigned CONFIG_KEY_LEN=20;
+const unsigned CONFIG_VALUE_LEN=100;
+const unsigned CONFIG_DESCR_LEN=100;
+
+struct JANEIN
+{
+  enum ETYPE
+	{
+	  FIRST__=-1,
+	  NEIN=0,
+	  JA=1,
+	  LAST__
+	};
+  static const std::string STYPES[];
+};
+
+template< class T > std::ostream & operator<<( std::ostream & out, const Tools::EnumRange<T> & jn )
+{
+  if( jn.value <= jn.FIRST__ || jn.value >= jn.LAST__ )
+	return out << jn.STYPES[0];
+  else
+	return out << jn.STYPES[jn.value];
+}
+
+template< class T > std::istream & operator>>( std::istream & in, Tools::EnumRange<T> & jn )
+{
+  std::string s;
+
+  in >> s;
+
+  for( int i = jn.FIRST__ + 1; i < jn.LAST__; i++ )
+	{
+	  if( s == jn.STYPES[i] ) 
+		{
+		  jn.value = static_cast<JANEIN::ETYPE>(i);
+		  break;
+		}
+	}
+  return in;
+}
+
+
+template<class T> bool is_bool( const Tools::EnumRange<T> & t ) { return false; }
+
+class TableType
+{
+ public:
+  enum Type
+	{
+	  MYISAM,
+	  INNODB
+	};
+
+  Type table_type;
+
+ public:
+  TableType( Type type = INNODB )
+	: table_type( type )
+	{}
+  virtual ~TableType() {}
+};
+
+class Forkey
+{
+ public:
+  const std::string own_table;
+  const std::string field;
+  const std::string target_table;
+  const std::string target_field;
+  
+  Forkey( DBBindType *base, 
+		  DBType *field_,
+		  const std::string & target_table_,
+		  const std::string & target_field_ );		  
+
+  virtual ~Forkey() {}
+};
+
+class Forkeys
+{
+ public:
+  std::vector< Ref<Forkey> > forkeys;
+
+  void add_key( Forkey *key )
+	{
+	  forkeys.push_back( key );
+	}
+};
+
+class BASE : public DBBindType, public TableType, public Forkeys
+{
+ public:
+  DBTypeInt      idx;
+  DBTypeEnum< EnumRange<JANEIN> > gueltig;
+  DBTypeDateTime gueltig_von;
+  DBTypeDateTime gueltig_bis;
+  DBTypeDateTime hist_an_zeit;
+  DBTypeVarChar  hist_an_user;
+  DBTypeDateTime hist_ae_zeit;
+  DBTypeVarChar  hist_ae_user;  
+  DBTypeDateTime hist_lo_zeit; unsigned count_erg( const std::string &table_name );
+  DBTypeVarChar  hist_lo_user;  
+
+  BASE( const std::string & name, DBBindType *base );
+
+  BASE & operator=( const BASE & b )
+	{ 
+	  DBBindType::operator=(b); 
+	  return *this;
+	}
+};
+
+class GRP_UST : public BASE
+{
+ public:
+  DBTypeVarChar name;
+  DBTypeVarChar bezeichnung;
+  DBTypeVarChar bezeichnung2;
+  DBTypeDouble  ust; // Umsatzsteuer
+  
+  GRP_UST();
+
+  GRP_UST & operator=( const GRP_UST & b )
+	{  
+	  BASE::operator=(b); 
+	  return *this;
+	}
+};
+
+class ART : public BASE
+{
+ public:
+  DBTypeVarChar artnr;
+  DBTypeVarChar bezeichnung;
+  DBTypeVarChar bezeichnung2;
+  DBTypeVarChar gruppe_ust;     /* Umsatzsteuergruppe */
+  DBTypeInt     gruppe_ust_idx;
+
+  ART();
+
+  ART & operator=( const ART & b )
+	{  
+	  BASE::operator=(b); 
+	  return *this;
+	}
+};
+
+class PREIS : public BASE
+{
+ public:
+  DBTypeVarChar art_artnr;
+  DBTypeInt     art_idx;
+  DBTypeDouble  preis;
+  DBTypeVarChar bezeichnung;
+  DBTypeVarChar bezeichnung2;
+  DBTypeEnum< EnumRange<JANEIN> > gutschein;
+  DBTypeEnum< EnumRange<JANEIN> > menuepreis; 
+
+  PREIS();
+  
+  PREIS & operator=( const PREIS & b )
+	{  
+	  BASE::operator=(b); 
+	  return *this;
+	}
+};
+
+class USER : public BASE
+{
+ public:
+  DBTypeVarChar login;
+  DBTypeVarChar vor_name;
+  DBTypeVarChar nach_name;
+  DBTypeVarChar passwd;
+
+  USER();
+
+  USER & operator=( const USER & b )
+	{  
+	  BASE::operator=(b); 
+	  return *this;
+	}
+};
+
+class GRP_ART : public DBBindType, public Forkeys
+{
+ public:
+  DBTypeInt     idx;
+  DBTypeVarChar name;
+  DBTypeVarChar bezeichnung;
+  DBTypeVarChar bezeichnung2;
+  DBTypeVarChar art_artnr; 
+  DBTypeInt     art_idx; 
+  
+  GRP_ART();
+
+  GRP_ART & operator=( const GRP_ART & b )
+	{  
+	  DBBindType::operator=(b); 
+	  return *this;
+	}
+};
+
+class LAYOUTK : public DBBindType
+{
+ public:
+  DBTypeInt      idx;
+  DBTypeVarChar  name;
+  DBTypeInt      width;
+  DBTypeInt      height;
+  DBTypeInt      color;
+
+  LAYOUTK();
+};
+
+class LAYOUTP : public DBBindType, public Forkeys
+{
+ public:
+  DBTypeInt      idx;
+  DBTypeInt      layout_idx;
+  DBTypeVarChar  label_name;
+  DBTypeInt      label_x;
+  DBTypeInt      label_y;
+  DBTypeInt      label_width;
+  DBTypeInt      label_height;
+  DBTypeInt      label_color;
+  DBTypeVarChar  label_icon;
+
+  LAYOUTP();
+};
+
+class BONLINE : public BASE
+{
+ public:
+  DBTypeVarChar  user;
+  DBTypeInt      user_idx;
+  DBTypeVarChar  artnr;
+  DBTypeInt      artnr_idx;
+  DBTypeEnum< EnumRange<JANEIN> > bill_printed;
+  DBTypeVarChar  bill_number;
+  DBTypeInt      layoutk_idx;
+  DBTypeVarChar  layoutk_name;
+  DBTypeInt      layoutp_idx;
+  DBTypeVarChar  layoutp_label_name;
+  DBTypeEnum< EnumRange<JANEIN> > tagesabschluss;
+  DBTypeDateTime abschl_zeit;
+  DBTypeInt      abschl_user_idx;
+  DBTypeVarChar  abschl_user_name;
+  DBTypeEnum< EnumRange<JANEIN> > storniert;
+  DBTypeVarChar  storno_user;
+  DBTypeInt      storno_user_idx;
+  DBTypeInt      anzahl;
+  DBTypeInt      preis_idx;
+  DBTypeDouble   preis;
+  DBTypeDouble   bar_bezahlt;
+  DBTypeDouble   sonst_bezahlt;
+  DBTypeVarChar  sonst_art_name;
+  DBTypeInt      sonst_art_idx;
+  DBTypeEnum< EnumRange<JANEIN> > bezahlt;
+
+  BONLINE();
+};
+
+class SONST_ART : public BASE
+{
+ public:
+  DBTypeVarChar  name; 
+  DBTypeVarChar  bezeichnung;
+  DBTypeVarChar  bezeichnung2;
+  DBTypeDouble   wert;
+
+  SONST_ART();
+};
+
+class LANG : public DBBindType, public TableType
+{
+ public:
+  DBTypeInt      idx;
+  DBTypeVarChar  system;
+  DBTypeVarChar  type;
+  DBTypeVarChar  orig;
+  DBTypeVarChar  trans;
+
+  LANG();
+};
+
+class CONFIG : public DBBindType, public TableType
+{
+ public:
+  DBTypeInt      idx;
+  DBTypeVarChar  system;
+  DBTypeVarChar  key;
+  DBTypeVarChar  value;
+  DBTypeVarChar  descr;
+
+  CONFIG();
+};
+
+std::string create_sql();
+std::string create_sql_statement( DBBindType *table );
+
+#endif
+
