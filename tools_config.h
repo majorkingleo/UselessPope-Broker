e3de83409c2905eb011281044cafa314cbe6ef42
Martin Oberzalek <kingleo@gmx.at>
2025-10-18 23:13:51 +0200

    copied from stips

--- tools_config.h
+++ tools_config.h
@@ -2,9 +2,9 @@
 #define TOOLS_CONFIG_H
 
 // #define TOOLS_USE_THREADS
-// #define TOOLS_USE_MYSQL
+#define TOOLS_USE_MYSQL
 // #define TOOLS_USE_ODBC
-// #define TOOLS_USE_DB
+#define TOOLS_USE_DB
 // #define TOOLS_USE_ORACLE
 #define TOOLS_USE_GUI
 
